5070961a2383c71bbb4a2337b0d8acfaaf56fcbc
HKEDU <HKEDU@DESKTOP-IHD16H3>
2023-12-07 16:19:25 +0900

    first commit.

---
+++ miniproject_searchNreserve/src/main/java/com/board/MiniprojectSearchNreserveApplication.java
@@ -0,0 +1,13 @@
+package com.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniprojectSearchNreserveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniprojectSearchNreserveApplication.class, args);
+	}
+
+}
