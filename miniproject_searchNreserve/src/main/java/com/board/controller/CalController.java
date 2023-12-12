9d7823c1f8036e399ef6fe45260882da62e2cfd0
HKEDU <HKEDU@DESKTOP-BTS1IFD>
2023-12-12 09:19:05 +0900

    머했는지기억안남

--- miniproject_searchNreserve/src/main/java/com/board/controller/CalController.java
+++ miniproject_searchNreserve/src/main/java/com/board/controller/CalController.java
@@ -77,7 +77,7 @@ public class CalController {
    }
    @PostMapping(value = "/addCalBoard")
    public String addCalBoard(@Validated InsertCalCommand insertCalCommand,
-                       BindingResult result) throws Exception {
+                       BindingResult result,int seq) throws Exception {
       logger.info("일정추가하기");
       System.out.println(insertCalCommand);
       if(result.hasErrors()) {
